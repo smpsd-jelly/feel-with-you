9e2152bfa8567bdf5a63458b2f66c09fad208ccb
smpsd-jelly <panipaksenmee@gmail.com>
2025-08-17 04:50:48 +0700

    fixed bugs and configs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
- matcher: [
+  matcher: [
     "/((?!api/auth|_next/static|_next/image|favicon.ico|images|login).*)",
   ],
 };
