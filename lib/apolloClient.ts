2c0ca8db47e6daef0dd50ada403bdc73b84cc280
smpsd-jelly <panipaksenmee@gmail.com>
2025-12-18 19:46:44 +0700

    add auth

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,17 +1,25 @@
-import { ApolloClient, InMemoryCache, ApolloLink } from '@apollo/client'
-import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
+import { ApolloClient, InMemoryCache, ApolloLink } from "@apollo/client";
+import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 
 const uploadLink = createUploadLink({
   uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/graphql",
-  credentials: 'include',
-  headers: {
-    "apollo-require-preflight": "true", 
-  },
-})
+  credentials: "include",
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHQL_TOKEN}`,
+      "apollo-require-preflight": "true",
+    },
+  }));
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  link: uploadLink as unknown as ApolloLink,
+  link: ApolloLink.from([authLink, uploadLink as unknown as ApolloLink]),
   cache: new InMemoryCache(),
-})
+});
 
-export default client
+export default client;
